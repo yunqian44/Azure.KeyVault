e6210ba492c2d09bc41d64594db8c02f3761c326
yunqian44 <yunqian8@live.com>
2020-10-23 16:57:41 +0800

    添加项目文件。

---
+++ Azure.KeyVault/Program.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+
+namespace Azure.KeyVault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
