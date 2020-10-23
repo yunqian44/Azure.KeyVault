23ec06ab087d6707981a2c4f4ad90e2f87ade9d1
yunqian44 <yunqian8@live.com>
2020-10-23 16:58:14 +0800

    Get Secret

--- Azure.KeyVault/Program.cs
+++ Azure.KeyVault/Program.cs
@@ -9,6 +9,16 @@ namespace Azure.KeyVault
     {
         static void Main(string[] args)
         {
+            string vaultUri = "https://cnbateblogweb-keyvault.vault.azure.net/";
+
+            var client = new SecretClient(vaultUri: new Uri(vaultUri), credential: new DefaultAzureCredential());
+
+            // Create a new secret using the secret client.
+            var secretResult = client.GetSecretAsync("CNBATEBLOGWEB-DB-CONNECTIONSTRING");
+
+            // Retrieve a secret using the secret client.
+            Console.WriteLine("azure key vault name of " + secretResult.Result.Value.Name + " is " + secretResult.Result.Value.Value);
+
             Console.ReadLine();
         }
     }
