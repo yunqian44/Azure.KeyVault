c4264926b68db287eb15705cbce50b9450d49680
yunqian44 <yunqian8@live.com>
2022-02-08 20:50:37 +0800

    Init Web Project

---
+++ Azure.KeyVault.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Azure.KeyVault.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
